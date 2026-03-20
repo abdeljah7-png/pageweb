0089cc23808f0bcbbd1b839a10f153dc4ad84a4a
Abdel Jah <abdeljah7-png@users.noreply.github.com>
2026-03-21 00:41:44 +0200

    urls

--- monsite/urls.py
+++ monsite/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import home, description, utilisation, download_dossier
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('/description/', description, name='description'),
-    path('/utilisation/', utilisation, name='utilisation'),  # <-- slash ajouté
-    path('/telecharger/', download_dossier, name='download_dossier'),
+    path('home/', home, name='home'),
+    path('description/', description, name='description'),
+    path('utilisation/', utilisation, name='utilisation'),  # <-- slash ajouté
+    path('telecharger/', download_dossier, name='download_dossier'),
 ]
