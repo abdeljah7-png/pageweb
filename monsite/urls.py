f77f81a3050da732549e807301fca8b74a2f7399
Abdel Jah <abdeljah7-png@users.noreply.github.com>
2026-03-21 01:09:06 +0200

    mise a jour pageweb

--- monsite/urls.py
+++ monsite/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import home, description, utilisation, download_dossier
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),  # page d'accueil principale
     path('description/', description, name='description'),
-    path('utilisation/', utilisation, name='utilisation'),  # <-- slash ajouté
+    path('utilisation/', utilisation, name='utilisation'),
     path('telecharger/', download_dossier, name='download_dossier'),
 ]
