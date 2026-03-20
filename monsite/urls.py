3b6611cc3d48744cb25627070b3dd17208b3967a
Abdel Jah <abdeljah7-png@users.noreply.github.com>
2026-03-21 00:35:30 +0200

    urls

--- monsite/urls.py
+++ monsite/urls.py
@@ -3,7 +3,7 @@ from .views import home, description, utilisation, download_dossier
 
 urlpatterns = [
     path('', home, name='home'),
-    path('description/', description, name='description'),
-    path('utilisation/', utilisation, name='utilisation'),  # <-- slash ajouté
-    path('telecharger/', download_dossier, name='download_dossier'),
+    path('/description/', description, name='description'),
+    path('/utilisation/', utilisation, name='utilisation'),  # <-- slash ajouté
+    path('/telecharger/', download_dossier, name='download_dossier'),
 ]
