3507c9168314313e1f5e9357822425092a444b52
Abdel Jah <abdeljah7-png@users.noreply.github.com>
2026-03-20 21:32:17 +0200

    change nom requirements.txt

--- pageweb/settings.py
+++ pageweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd0$u3ur!^*k#1zm8-!ikgk7kf-!(29d791u4_$br49tp*dfl=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOST = ['*']
